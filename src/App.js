4099d6a36ee5f20c20d47fa7e3186e3a07420132
unknown <crsn1111@gmail.com>
2021-08-24 18:12:45 +0900

    finish

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import React from "react";
+import styled from "styled-components";
 import RamdomNumber from "./components/RamdomNumber";
+
+const Body = styled.div`
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const App = () =>{
   return (
-    <RamdomNumber></RamdomNumber>
+    <Body>
+      <RamdomNumber></RamdomNumber>
+    </Body>
+    
   );
 }
 
--- src/components/Box.js
+++ src/components/Box.js
@@ -6,12 +6,16 @@ const Box = ({num, handleClick}) =>{
 }
 
 const Container = styled.div`
-  border: 1px solid red;
+  border: 1px solid rgba(0,0,0,0.8);
   display: flex;
   justify-content: center;
   align-items: center;
   font-size: 40px;
   font-weight: 600;
+  cursor: pointer;
+  &:hover {
+    box-shadow: 0px 0px 10px rgba(0,0,0,0.8) inset, 0px 0px 5px rgba(200,200,200,0.5);
+  }
 `;
 
 export default Box;
